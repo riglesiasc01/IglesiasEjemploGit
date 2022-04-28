286736037dbe8354e1b2ca31531c3161c84057a4
damt114 <damt114@36-10.aglinformatica.es>
2022-04-28 18:06:58 +0200

    Version 1

---
+++ src/ejemplo/Principal.java
@@ -0,0 +1,10 @@
+package ejemplo;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Raul Iglesias Carrera");
+        System.out.println("Version 1");
+    }
+
+}
