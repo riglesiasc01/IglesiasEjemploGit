5dc600fb4336e6075b218f6b34226b642dd0bf98
damt114 <damt114@36-10.aglinformatica.es>
2022-04-28 19:17:58 +0200

    Version 1

---
+++ IglesiasEjemploGit/src/iglesiasejemplogit/IglesiasEjemploGit.java
@@ -0,0 +1,13 @@
+
+package iglesiasejemplogit;
+
+
+public class IglesiasEjemploGit {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Raul Iglesias Carrera");
+        System.out.println("Version 1");
+    }
+    
+}
